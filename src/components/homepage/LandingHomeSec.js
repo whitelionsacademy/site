aabc3a2f4b0cdbdb7fd1b2de64341cfd14e7afc4
tomiwa_salau <ayotomiwasalau@gmail.com>
2023-03-19 16:40:38 +0100

    homepage update

--- src/components/homepage/LandingHomeSec.js
+++ src/components/homepage/LandingHomeSec.js
@@ -4,7 +4,7 @@ import landimg from '../media/landimg.jpeg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
-function LandingSec(){
+function LandingHomeSec(){
     return (
         <div class="landingsec">
             <NavSec/>
@@ -12,7 +12,7 @@ function LandingSec(){
                 <h1 className="headertext" >WhiteLions Basketball Academy</h1>
                 <button id="btn1" class="btn btn-light btn-lg">Explore</button>
                 <button id="btn2" class="btn btn-light btn-lg"> Contact us</button>
-                <img className="landimg"></img>
+                <img className="landhomeimg"></img>
             </div>
 
             
@@ -20,4 +20,4 @@ function LandingSec(){
     )
 }
 
-export default LandingSec;
+export default LandingHomeSec;
--- src/components/homepage/programSec.js
+++ src/components/homepage/programSec.js
@@ -7,15 +7,37 @@ function ProgramSec(){
 
             <div class="progsubsec">
 
-                <h2>Programs</h2>
-                <h3>Trainings</h3>
+                <div class="expheader">
+                    <h2 class="progtext txt1">Programs</h2>
+                    <h3 class="progtext txt2">Trainings</h3>
+                </div>
 
                 <div class="progone">
 
                     <img src={progimg} class="rounded float-right progimg"></img>
                     <div class="progdiv">
-                        <p>Duis rutrum lacus eu lacus varius commodo. Pellentesque lobortis tellus a lacus dictum molestie. Sed sit amet augue euismod, dictum risus at, vestibulum libero. Vestibulum aliquam arcu sapien, in lacinia erat pharetra sit amet. Sed lacinia libero ante, eget pretium magna tempus in. </p>
-                        <div class="strip"></div>
+                        <p class='ptag'>Duis rutrum lacus eu lacus varius commodo. Pellentesque lobortis tellus a lacus dictum molestie. Sed sit amet augue euismod, dictum risus at, vestibulum libero. Vestibulum aliquam arcu sapien, in lacinia erat pharetra sit amet. Sed lacinia libero ante, eget pretium magna tempus in. </p>
+                        <div class="pstrip"></div>
+                    </div>
+                    
+                </div>
+
+                <div class="progone" id="progonemiddle">
+
+                    <img src={progimg} class="rounded float-left progimg"></img>
+                    <div class="progdiv" id="progdivmiddle">
+                        <p class='ptag'>Duis rutrum lacus eu lacus varius commodo. Pellentesque lobortis tellus a lacus dictum molestie. Sed sit amet augue euismod, dictum risus at, vestibulum libero. Vestibulum aliquam arcu sapien, in lacinia erat pharetra sit amet. Sed lacinia libero ante, eget pretium magna tempus in. </p>
+                        <div class="pstrip"></div>
+                    </div>
+                    
+                </div>
+
+                <div class="progone">
+
+                    <img src={progimg} class="rounded float-right progimg"></img>
+                    <div class="progdiv">
+                        <p class='ptag'>Duis rutrum lacus eu lacus varius commodo. Pellentesque lobortis tellus a lacus dictum molestie. Sed sit amet augue euismod, dictum risus at, vestibulum libero. Vestibulum aliquam arcu sapien, in lacinia erat pharetra sit amet. Sed lacinia libero ante, eget pretium magna tempus in. </p>
+                        <div class="pstrip"></div>
                     </div>
                     
                 </div>
