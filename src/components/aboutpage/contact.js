4cf1fd664148eb706c552aaeeb3780f5ae8b4c8b
TemitopeSamuel <opeyemitopebakare@gmail.com>
2023-04-11 21:33:50 +0100

    Additional update on the about page

--- src/components/aboutpage/contact.js
+++ src/components/aboutpage/contact.js
@@ -11,20 +11,25 @@ function ExperienceSec(){
     return (
         <div class="contact">
             <div class="contacts">
+                <div class="contactf">
+                    <h3>Content</h3><br class="brs" />
+                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Mollitia nulla dolores soluta tenetur omnis.</p>
+                    <p class="cont">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure, maiores! Vero reprehenderit tempora aperiam accusantium nihil earum qui, quasi aspernatur, dignissimos culpa quidem corporis, odio id mollitia aliquid pariatur voluptatibus!</p>
+                </div>
                 <div class="contactflex">
-                    <h1>Contact Us</h1>
-                    <h3>Lagos Nigeria</h3>
-                    <h3>10 Lambo Iasunwo firstgate opp. Laspotech Ikorodu North Lagos NG, 100001, Ikorodu.</h3>
+                    <h3>About</h3><br class="brs" />
+                    <a class="nav-link" href="#">Home</a><br />
+                    <a class="nav-link" href="#">Programme</a><br />
+                    <a class="nav-link" href="#">Gallery</a><br />
+                    <a class="nav-link" href="#">Contact</a><br />
                 </div>
                 <div class="quick">
-                    <h1>Quick Links</h1>
-                    <a class="nav-link" href="#">Home</a>
-                    <a class="nav-link" href="#">Programme</a>
-                    <a class="nav-link" href="#">Gallery</a>
-                    <a class="nav-link" href="#">Contact</a>
+                    <h3>Address</h3><br class="brs" />
+                    <p>Lagos Nigeria</p>
+                    <p>10 Lambo Iasunwo firstgate opp. Laspotech Ikorodu North Lagos NG, 100001, Ikorodu.</p>
                 </div>
                 <div class="icons">
-                    <h1>Follow Us</h1>
+                    <h3>Follow Us</h3>
                     <a href="#"><img src={faceimg} className="faceimg" /></a>
                     <a href="#"><img src={twittimg} className="twittimg" /></a>
                     <a href="#"><img src={instaimg} className="instaimg" /></a>
@@ -32,7 +37,7 @@ function ExperienceSec(){
                 </div>
             </div>
             <div class="copyright">
-                <h2>©2023 whiteLions Basketball Academy</h2>
+                <h3>©2023 whiteLions Basketball Academy</h3>
             </div>
         </div>    
     );
