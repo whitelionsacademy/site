5a7799594d0ebb1318c3d6053ec8bba38d95ad3b
ayotomiwasalau <ayotomiwasalau@gmail.com>
2023-05-14 03:23:43 +0100

    update for deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
 
       <Routes>
           <Route path="/" element={<Homepage/>}/>
