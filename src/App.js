a98790cfb3e2fa2792a968ddf7dc838099fba884
ayotomiwasalau <ayotomiwasalau@gmail.com>
2023-05-20 22:35:42 +0100

    router fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import './App.css';
 import Homepage from './components/Homepage.js';
 import Aboutpage from './components/Aboutpage.js';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import {BrowserRouter, HashRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
 
       <Routes>
           <Route path="/" element={<Homepage/>}/>
           <Route path="/about" element={<Aboutpage/>}/> 
       </Routes>
 
-    </BrowserRouter>
+    </HashRouter>
     
     
   );
