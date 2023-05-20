a41e25eafe385c3f5129080b0d291596ef346173
ayotomiwasalau <ayotomiwasalau@gmail.com>
2023-05-20 11:34:57 +0100

    update nav router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter>
 
       <Routes>
           <Route path="/" element={<Homepage/>}/>
