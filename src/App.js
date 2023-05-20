3fa944fe530f843d48466b082e09b174cbd7ce58
ayotomiwasalau <ayotomiwasalau@gmail.com>
2023-05-21 00:39:33 +0100

    router fix

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import {BrowserRouter, HashRouter,Routes, Route} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <BrowserRouter basename="/wlion">
 
       <Routes>
           <Route path="/" element={<Homepage/>}/>
