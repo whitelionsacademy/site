a79884a96a8cd45d6ae3cc4a592073cd505ef118
ayotomiwasalau <ayotomiwasalau@gmail.com>
2023-05-21 12:56:33 +0100

    lannding button update

--- src/components/homepage/LandingHomeSec.js
+++ src/components/homepage/LandingHomeSec.js
@@ -2,6 +2,8 @@ import React from "react";
 import NavSec from "./NavSec.js"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
+import { Link } from "react-router-dom";
+
 
 function LandingHomeSec(){
     return (
@@ -9,7 +11,7 @@ function LandingHomeSec(){
             <NavSec/>
             <div className="landingsecmid">
                 <h1 className="headertext" >WhiteLions Basketball Academy</h1>
-                 <a href="/about"><button id="btn1" class="btn btn-light btn-lg">About us</button></a>
+                 <Link class="nav-link" to="/"><button id="btn1" class="btn btn-light btn-lg">About us</button></Link>
                 {/* <button id="btn2" class="btn btn-light btn-lg"> Explore</button> */}
                 <img className="landhomeimg"></img>
             </div>
