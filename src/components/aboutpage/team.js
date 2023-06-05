eb45306be9c745ea71b9915d871cd2c2b929bdc3
Opeyemi Samuel <opeyemitopebakare@gmail.com>
2023-06-05 22:07:33 +0100

    update and adjustment on about page

--- src/components/aboutpage/team.js
+++ src/components/aboutpage/team.js
@@ -1,5 +1,5 @@
 import React from "react";
-import coachimg from '../media/image_co.jpg'
+import teaimg from '../media/image_co.jpg'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 
@@ -8,16 +8,18 @@ function ExperienceSec(){
     return (
         <div class = "team">
             <div class="teamflex">
-                <h2 class="text tex">Our Team</h2>
-                <div class="teamco">
-                    <h3 className="name text tex1">Oyewole Adebiyi John <br />Head Coach</h3>
-                    <p class="teamparag Parag">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                    Rerum ut voluptates quisquam quidem similique modi impedit.</p> 
+                <div class="teamsection">
+                    <div class="teamwrt">
+                    <h2 class="text tex">Our Team</h2>
+                    <br/>
+                        <h3 className="name text tex1">Oyewole Adebiyi John <br />Head Coach</h3>
+                        <p class="teamparag Parag">Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Rerum ut voluptates quisquam quidem similique modi impedit.</p> 
+                    </div>
+                    <div class="teaimge">
+                    <img src={teaimg} className="rounded teaimg"/>
+                    </div>
                 </div>
-                <div class="coachimge">
-                <img src={coachimg} className="rounded coachimg"/>
-                </div>
-                
             </div>
         </div>    
     );
